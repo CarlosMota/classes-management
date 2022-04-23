2c5f51004bd962d8561e59b5cdd0ccf0b8dcabeb
Carlos Silva <carlos.motta89@gmail.com>
2022-04-22 22:24:30 -0400

    [API] Add modules
    * Added auth module, controller and service
    * Added schedules module, controller and service
    
    Signed-off-by: Carlos Silva <carlos.motta89@gmail.com>

--- classes_management.api/src/app.module.ts
+++ classes_management.api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SchedulesModule } from './schedules/schedules.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
   providers: [AppService],
+  imports: [SchedulesModule, AuthModule],
 })
 export class AppModule {}
